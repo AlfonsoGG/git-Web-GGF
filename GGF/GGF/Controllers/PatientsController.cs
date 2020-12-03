68cc864a72d83979975655b89aece363902932dc
DGGlez <67668513+DGGlez@users.noreply.github.com>
2020-12-02 22:05:01 -0600

    DGG agregado de proyecto web

---
+++ GGF/GGF/Controllers/PatientsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GGF.Controllers
+{
+    public class PatientsController : Controller
+    {
+        // GET: Patients
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Edit(int id = 1)
+        {
+            return View();
+        }
+
+        public ActionResult DataShow(int id = 1)
+        {
+            return View();
+        }
+
+    }
+}
